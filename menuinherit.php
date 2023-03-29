69534f3e1eac5b41dc1bfcbc6713843f279b8817
Andy Kirk <andy@andykirk.net>
2023-03-29 10:42:58 +0100

    Use correct db function.

--- menuinherit.php
+++ menuinherit.php
@@ -158,7 +158,7 @@ class plgContentMenuInherit extends JPlugin
             $query = trim($query, ',');
 
             $db->setQuery($query);
-            $db->loadResult();
+            $db->execute();
         }
 	}
 
